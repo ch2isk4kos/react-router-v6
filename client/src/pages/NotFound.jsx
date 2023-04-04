f16f93e04bdd600b3318799004f8a1be3f50189b
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-04 15:44:17 -0400

    create component for not found page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NotFound = () => {
   return (
     <div>
-      <h1>Not Found Page</h1>
+      <h1>Page Not Found</h1>
       <em>This page doesn't exist.</em>
     </div>
   );
