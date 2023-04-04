45bc7088e5bdbbe010a536c50ba63d3e27c1cf9e
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-04 15:41:44 -0400

    import stylesheet

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
+import "../../styles/Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="Navbar">
-      <Link to="/">LOGO</Link>
-      <ul className="Navbar">
-        <li>
-          <Link to="/posts">Posts</Link>
+      <Link id="logo" to="/">
+        LOGO
+      </Link>
+      <ul className="nav-links">
+        <li className="link">
+          <Link to="/private">Private</Link>
         </li>
-        <li>
+        <li className="link">
           <Link to="/login">Login</Link>
         </li>
       </ul>
