9fbe2b9e6a56b4a4bedf5dba0e70bf4211732ae5
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-04 13:26:18 -0400

    create pages directory && component for notfound page

---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Not Found Page</h1>
+      <em>This page doesn't exist.</em>
+    </div>
+  );
+};
+
+export default NotFound;
